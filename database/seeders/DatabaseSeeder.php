9557100e5454b5d8cc0259322621cd42ee58ef02
timubin <mubin3242@gmail.com>
2024-06-20 21:08:25 +0600

    Model Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\student;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,9 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
+
+        // student::factory()->count(20)->create();
+        student::factory(10)->create();
+
+
+
+      /*   $this->call([
             StudentSeeder::class,
            
-        ]);
+        ]); */
     }
 }
