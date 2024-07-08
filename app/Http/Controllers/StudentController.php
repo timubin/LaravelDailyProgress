2c9636df9d24510dfda139a50f7e81f776d6db7d
timubin <mubin3242@gmail.com>
2024-07-08 22:33:11 +0600

    Query Builder:Union-Chunk-When Method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -28,4 +28,27 @@ class StudentController extends Controller
                     // return $students;
             return view('home', compact('students'));
     }
+
+    public function uniondata(){
+       $lecturers = DB::table('lecturers')->select('name' , 'email' , 'city_name')->join('cities', 'lecturers.city', '=', 'cities.id');
+
+       $students = DB::table('students')->select('name' , 'email', 'city_name')
+        ->union($lecturers)->join('cities', 'students.city', '=', 'cities.id')
+        /* ->toSql() */
+        ->get();
+
+        return $students;
+    }
+
+public function chunkdata(){
+    $students = DB::table('students')->orderBy('id')
+    ->chunk(3, function($students){
+        echo "<div style='border: 1px solid black; margin-bottom: 15px;'>";
+        foreach($students as $student){
+            echo $student->name . "<br>";
+        };
+        echo "</div>";
+    });
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[StudentController::class , 'showStudents']);
 
-
+Route::get('uniondata',[StudentController::class , 'uniondata']);
+Route::get('chunk',[StudentController::class , 'chunkdata']);
 
